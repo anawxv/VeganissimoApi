55142e617c5b2298fa8a48df8312e8a6027efaae
Ana Vieira <analirajonhson@gmail.com>
2023-12-03 21:09:27 -0300

    unit

---
+++ Vegan.api/Repositories/Unit of Work/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Vegan.api.Repositories.Unit_of_Work
+{
+    public interface IUnitOfWork
+    {
+        public interface IUnitOfWork
+        {
+            public Task SaveChangesAsync();
+        }
+    }
+}
---
+++ Vegan.api/Repositories/Unit of Work/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vegan.api.Data;
+
+namespace Vegan.api.Repositories.Unit_of_Work
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _dataContext;
+
+        public UnitOfWork(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
